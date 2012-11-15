ebee83a06aa236c3873aa32464d16a336c934c3b
Andy Leonard <andy@hurricane-ridge.com>
2012-11-15 11:50:05 -0800

    Initial boilerplate commit.

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# == Class: bind
+#
+# Module to install and configure ISC BIND.
+#
+# Currently tested/supported on Ubuntu 12.04.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+#
+# === Examples
+#
+#  include bind
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard, Seattle Biomedical Research Institute
+#
+class bind {
+
+}
