7c6d402eae03ff3b770a906a9291deeded68186a
Andrew Leonard <andy@hurricane-ridge.com>
2012-12-27 06:58:51 -0800

    Make default urls param an empty hash; complete PuppetDoc.

--- manifests/install_pkg.pp
+++ manifests/install_pkg.pp
@@ -2,17 +2,26 @@
 #
 # Download and install a dpkg from an arbitrary location.
 #
-# This could probably live in another module, but, for now, is here.
+# This may ultimately move to another module to stay DRY, but, for now, is
+# here.
 #
 # === Parameters
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*provider*]
+#   Package provider to use; defaults to "dpkg".
+#
+# [*urls*]
+#   Hash associating "package name" => "package url"; used for downloading the
+#   correct package from an arbitrary URL.  This is a hash instead of just a
+#   string to facilitate the calling class installing multiple packages from
+#   a hash itself.
 #
 # === Examples
 #
-#
+# bind::install_pkg { 'bind':
+#   urls =>
+#     { 'bind' => 'https://repo.hurricane-ridge.com/bind-9.9.2-3_amd64.deb' },
+# }
 #
 # === Authors
 #
@@ -24,7 +33,7 @@
 #
 define bind::install_pkg(
   $provider = 'dpkg',
-  $urls = []
+  $urls = {}
   ) {
 
   exec { "Download ${name}":
